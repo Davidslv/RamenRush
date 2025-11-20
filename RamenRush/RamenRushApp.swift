22fa29b31831dc28077c5f5a1beaf6b7ced11acc
David Silva <davidslv@users.noreply.github.com>
2025-11-20 19:59:05 +0000

    Initial Commit

---
+++ RamenRush/RamenRushApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamenRushApp.swift
+//  RamenRush
+//
+//  Created by David Silva on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct RamenRushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RamenRushTests/RamenRushTests.swift
@@ -0,0 +1,16 @@
+//
+//  RamenRushTests.swift
+//  RamenRushTests
+//
+//  Created by David Silva on 20/11/2025.
+//
+
+import Testing
+
+struct RamenRushTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
